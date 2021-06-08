2b946b699a1db3b2af283082d3226f6af4988bc6
Kristianto Haryodi <34808653+KrisHaryodi@users.noreply.github.com>
2021-06-08 22:14:10 +0700

    Update integrasi.php

--- integrasi.php
+++ integrasi.php
@@ -1,6 +1,6 @@
 <?php
 $api_url = 'https://go-tukang.herokuapp.com/tukang/list';
-
+include('koneksi.php');
 //Read Json files
 $json_data = file_get_contents($api_url);
 //Decode Json data into PHP array
@@ -8,29 +8,11 @@ $response_data = json_decode($json_data);
 $list = $response_data->list;
 
 foreach($list as $lis){
-    echo "price: ".$lis->price;
+    echo "username: ".$lis->username;
     echo "<br/> ";
+    $nama = $lis->username;
+    $query = mysqli_query($koneksi,"INSERT INTO `admin` (`nama`) VALUES ('$nama')");
 };
-//include('dbconnected.php');
-include('koneksi.php');
-
-$tgl_pemasukan = $_GET['tgl_pemasukan'];
-$jumlah = $_GET['jumlah'];
-$sumber = 'Go-helper';
-
-//query update
-$query = mysqli_query($koneksi,"INSERT INTO `pemasukan` (`tgl_pemasukan`, `jumlah`, `id_sumber`) VALUES ('$tgl_pemasukan', '$jumlah', '$sumber')");
-
-if ($query) {
- # credirect ke page index
- header("location:pendapatan.php"); 
-}
-else{
- echo "ERROR, data gagal diupdate". mysqli_error($koneksi);
-}
-
-//mysql_close($host);
-
 ?>
 
 
