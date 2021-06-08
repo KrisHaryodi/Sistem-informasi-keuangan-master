25aa56b12f2eea129832d6f47163f7c0c50d0ac9
Kristianto Haryodi <34808653+KrisHaryodi@users.noreply.github.com>
2021-06-08 21:43:35 +0700

    Update integrasi.php

--- integrasi.php
+++ integrasi.php
@@ -8,11 +8,29 @@ $response_data = json_decode($json_data);
 $list = $response_data->list;
 
 foreach($list as $lis){
-    echo "username: ".$lis->username;
-    echo "<br/> ";
     echo "price: ".$lis->price;
     echo "<br/> ";
 };
+//include('dbconnected.php');
+include('koneksi.php');
+
+$tgl_pemasukan = $_GET['tgl_pemasukan'];
+$jumlah = $_GET['jumlah'];
+$sumber = 'Go-helper';
+
+//query update
+$query = mysqli_query($koneksi,"INSERT INTO `pemasukan` (`tgl_pemasukan`, `jumlah`, `id_sumber`) VALUES ('$tgl_pemasukan', '$jumlah', '$sumber')");
+
+if ($query) {
+ # credirect ke page index
+ header("location:pendapatan.php"); 
+}
+else{
+ echo "ERROR, data gagal diupdate". mysqli_error($koneksi);
+}
+
+//mysql_close($host);
+
 ?>
 
 
